b8a8c2d40fcadfa1dd35a22366429d4f46ee63ce
Kara Cavanaugh <karacav@msn.com>
2019-07-27 20:58:16 -0400

    Destructuring 1

--- part1.js
+++ part1.js
@@ -141,3 +141,14 @@ temp = thermos.temperature; // 26 in C
 import { capitalizeStrings } from "string_functions"
 import * as capitalize_strings from "capitalize_strings";
 export { capitalizeString, foo }
+
+// 16. ES6: Use Destructuring Assignment to Assign Variables from Objects - obtain the average temp for
+// tomorrow from the input object AVG_TEMPERATURES, and assign value with key tomorrow to tempOfTomorrow in line.
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+function getTempOfTmrw(avgTemperatures) {
+  const {tomorrow : tempOfTomorrow} = avgTemperatures;
+  return tempOfTomorrow;
+}
