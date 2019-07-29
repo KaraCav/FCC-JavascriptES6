bb3fd09fff7216fbd776e0a12fef1c76260d277e
Kara Cavanaugh <karacav@msn.com>
2019-07-29 16:05:53 -0400

    Number 2
    Destructuring Assignment to Pass an Object as a Function's Parameters

--- part2.js
+++ part2.js
@@ -1,4 +1,4 @@
-// #1 ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
+// 1. ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
 const source = [1,2,3,4,5,6,7,8,9,10];
 function removeFirstTwo(list) {
   const [a,b, ...arr] = list; 
@@ -6,3 +6,18 @@ function removeFirstTwo(list) {
 }
 const arr = removeFirstTwo(source);
 console.log(arr); // should be [3,4,5,6,7,8,9,10]
+
+// 2. ES6: Use Destructuring Assignment to Pass an Object as a Function's Parameters
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  min: -0.75,
+  average: 35.85
+};
+const half = (function() {
+    return function half({max, min}) {
+    return (stats.max + stats.min) / 2.0;
+  };
+})();
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
