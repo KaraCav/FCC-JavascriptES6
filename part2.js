f7c6ba9a3cac87eaf4030348917c7d6e1c5f6d79
Kara Cavanaugh <karacav@msn.com>
2019-07-29 15:52:46 -0400

    Create part2.js

---
+++ part2.js
@@ -0,0 +1,8 @@
+// #1 ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  const [a,b, ...arr] = list; 
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
